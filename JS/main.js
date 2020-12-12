3fddd247db59426f9f7639b334c939bc7eba928c
Sabrina <sanutaayvi@gmail.com>
2020-12-12 21:36:07 +0600

    print

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+console.log("Minhaz+ Nusrat");
+
+var  numbr = 5; //in-line comment
+
+/*this  is a 
+  multiline  comment*/
+
+//   Data  Type   and   variables
+
+
+  /* Data types:
+  undefiend,  null,  boolean,  string,  symbol,  number,
+  and  object */
+  
