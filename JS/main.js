f09193ff2667bfd2e6d1fafd164d248b9a68ea5c
Sabrina <sanutaayvi@gmail.com>
2020-12-13 12:26:53 +0600

    var  initialization

--- JS/main.js
+++ JS/main.js
@@ -11,4 +11,44 @@ var  numbr = 5; //in-line comment
   /* Data types:
   undefiend,  null,  boolean,  string,  symbol,  number,
   and  object */
-  
+  var  myName  = "Sabrina" /// can  change
+  myName = 8
+
+  let ourName  = "SabMin"
+
+  const pi = 3.14 //can never  change
+
+  var a;
+  console.log(a);
+
+  var  b = 2;
+
+a = 5;
+b= a;
+console.log(a);
+console.log(a);
+
+//initializing  variable
+
+var a = 9;
+var   b = 4;
+var  c = "I  am  a "
+
+a = a+1;
+b = b+1;
+c = c+ "String!"
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+
+
+
+
+
+
+
+
+
